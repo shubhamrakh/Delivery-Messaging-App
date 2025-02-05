404acd8ab521ce4ffa48c4586e21a9d11c3f96d0
Shubham Rakh <shubham.rakh35@gmail.com>
2025-02-05 18:34:13 +0530

    Add files via upload

---
+++ endUser/src/main/java/com/endUser/AppConstants.java
@@ -0,0 +1,9 @@
+package com.endUser;
+
+public class AppConstants {
+	
+	public static final String LOCATION_UPDATE_TOPIC="location-update-topic";
+	
+	public static final String GROUP_ID="GROUP-1";
+
+}
